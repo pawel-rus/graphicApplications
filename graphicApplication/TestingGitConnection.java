eeacb144f52f3e0ba541a6520d3bf18beee1c790
Pawel Rus <pawelrus.637@gmail.com>
2023-12-23 17:52:05 +0100

    first changes

--- graphicApplication/TestingGitConnection.java
+++ graphicApplication/TestingGitConnection.java
@@ -3,7 +3,7 @@ package graphicApplication;
 public class TestingGitConnection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Git status?!");
 
 	}
 

c19be962807b68a54f55d43d1cf7efe3a09bf955
Pawel Rus <pawelrus.637@gmail.com>
2023-12-24 23:25:57 +0100

    override actionPerformed

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -18,20 +18,21 @@ public class TicTacToe implements ActionListener{
 	
 	TicTacToe(){
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800,800);
+		frame.setSize(600,600);
 		frame.getContentPane().setBackground(new Color(65, 65, 65));
 		frame.setLayout(new BorderLayout());
 		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 		
 		textfield.setBackground(new Color(30, 30, 30));
 		textfield.setForeground(new Color(25, 255, 0));
-		textfield.setFont(new Font("Ink Free",Font.BOLD,75));
+		textfield.setFont(new Font("Helvetica",Font.BOLD,65));
 		textfield.setHorizontalAlignment(JLabel.CENTER);
-		textfield.setText("TIC-TAC-TOE");
+		textfield.setText("tic-tac-toe");
 		textfield.setOpaque(true);
 		
 		title_panel.setLayout(new BorderLayout());
-		title_panel.setBounds(0, 0, 800, 100);
+		title_panel.setBounds(0, 0, 600, 80);
 		
 		button_panel.setLayout(new GridLayout(3,3));
 		button_panel.setBackground(new Color(150, 150, 150));
@@ -48,15 +49,47 @@ public class TicTacToe implements ActionListener{
 		frame.add(title_panel, BorderLayout.NORTH);
 		frame.add(button_panel);
 		
+		firstTurn();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		for(int i=0; i<9; i++) {
+			if(e.getSource() == buttons[i]) {
+				if(xTurn) {
+					if(buttons[i].getText() == "") {
+						buttons[i].setForeground(new Color(0,0,255));
+						buttons[i].setText("X");
+						xTurn = false;
+						textfield.setText("O turn");
+						check();
+					}
+				} else {
+					if(buttons[i].getText() == "") {
+						buttons[i].setForeground(new Color(255,0,0));
+						buttons[i].setText("O");
+						xTurn = true;
+						textfield.setText("X turn");
+						check();
+					}
+				}
+			}
+		}
 	}
 	
 	public void firstTurn() {
-		
+		try {
+			Thread.sleep(4000);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		if(random.nextInt(2) == 1) {
+			xTurn = true;
+			textfield.setText("X turn");
+		} else {
+			xTurn = false;
+			textfield.setText("O turn");
+		}
 	}
 	
 	public void check() {

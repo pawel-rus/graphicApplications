2b8473cb8bc82e00d40c2a9729f28014b6f352df
Pawel Rus <pawelrus.637@gmail.com>
2023-12-28 01:06:57 +0100

    testing log levels

--- graphicApplication/TestingGitConnection.java
+++ graphicApplication/TestingGitConnection.java
@@ -1,10 +1,16 @@
 package graphicApplication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class TestingGitConnection {
 
+	private static Logger demoLogger = LogManager.getLogger(TestingGitConnection.class.getName());
 	public static void main(String[] args) {
-		System.out.println("Git status?!");
-
+		
+		demoLogger.info("Click successfull!");
+		demoLogger.error("DB Conection failed");
+		
 	}
-
 }

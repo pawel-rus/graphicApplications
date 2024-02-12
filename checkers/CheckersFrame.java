1ad100f5bb6b74ace6a25496dc9821f710b734ff
Pawel Rus <pawelrus.637@gmail.com>
2024-02-12 01:15:17 +0100

    adding pieces moves, blocking some invalid moves

--- checkers/CheckersFrame.java
+++ checkers/CheckersFrame.java
@@ -1,7 +1,9 @@
 package checkers;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -13,21 +15,27 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import snake.SnakePanel;
 
 public class CheckersFrame extends JFrame implements ActionListener{
+	JPanel turnPanel = new JPanel();
+	JTextField turnField;
 	JButton[][] board = new JButton[8][8];
 	JPanel boardPanel = new JPanel();
 	String blackPiece = "resources/black.png";
 	String whitePiece = "resources/white.png";
+	String turn = whitePiece;
 	private int selectedRow = -1;
     private int selectedCol = -1;
     private int destinationRow;
     private int destinationCol;
 	CheckersFrame(){
 		initBoard();
-		this.add(boardPanel);
+		this.setLayout(new BorderLayout());
+		this.add(turnPanel, BorderLayout.NORTH);
+		this.add(boardPanel,BorderLayout.SOUTH);
 		this.setTitle("Checkers");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
@@ -37,6 +45,15 @@ public class CheckersFrame extends JFrame implements ActionListener{
 	}
 	
 	public void initBoard(){
+		turnField = new JTextField("WHITE's turn");
+		turnField.setEditable(false);
+		turnField.setHorizontalAlignment(JTextField.CENTER);
+		turnField.setBackground(new Color(139, 69, 19));
+		turnField.setForeground(new Color(240, 220, 130));
+		turnField.setFont(new Font("Arial", Font.BOLD, 20));
+		turnPanel.setLayout(new BorderLayout());
+		turnPanel.add(turnField, BorderLayout.CENTER);
+		//turnPanel.setBackground(Color.LIGHT_GRAY);
 		boardPanel.setLayout(new GridLayout(8,8));
 		for (int i = 0; i < 8; i++) {
 		    for (int j = 0; j < 8; j++) {
@@ -77,14 +94,25 @@ public class CheckersFrame extends JFrame implements ActionListener{
 	        for (int j = 0; j < 8; j++) {
 	            if (e.getSource() == board[i][j]) {
 	                if(selectedRow == -1 && selectedCol == -1) {
-	                	selectedRow = i;
+	                	if (isValidPiece(i, j)) {
+	            		selectedRow = i;
                         selectedCol = j;
+	                	}else {
+	                        System.out.println("Invalid piece. Select a valid piece.");
+	                	}
 	                }else {
 	                	destinationRow = i;
 	                	destinationCol = j;
 	                	handleMove(selectedRow, selectedCol, destinationRow, destinationCol);
 	                	selectedRow = -1;
                         selectedCol = -1;
+                        if(turn.equals(whitePiece)) {
+                        	turn = blackPiece;
+                        	turnField.setText("BLACK's turn");
+                        }else {
+                        	turn = whitePiece;
+                        	turnField.setText("WHITE's turn");
+                        }
 	                }
 	                return; 
 	            }
@@ -102,7 +130,15 @@ public class CheckersFrame extends JFrame implements ActionListener{
 	            System.out.println("Moving BLACK piece from (" + startRow + ", " + startCol + ") to (" + destRow + ", " + destCol + ").");
 	            setCheckerPiece(destRow, destCol, blackPiece);
 	        }
+	        board[startRow][startCol].setIcon(null);
 	    }
 				
+	}
+	
+	private boolean isValidPiece(int row, int col) {
+	    ImageIcon selectedIcon = (ImageIcon) board[row][col].getIcon();
+	    //String currentPlayerPiece = (turn.equals("WHITE")) ? whitePiece : blackPiece;
+
+	    return (selectedIcon != null && selectedIcon.getDescription().equals(turn));
 	}
 }
